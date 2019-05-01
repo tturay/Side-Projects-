2b1aae4298056a9e69f052a29367e20e6c033603
tturay <43738593+tturay@users.noreply.github.com>
2019-04-30 23:44:03 -0400

    Add files via upload

---
+++ Personal Projects/newflaskapp/flaskblogg.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, url_for #importing flask class
+#from forms import RegistrationFormm, LoginForm
+app = Flask(__name__) #app variable is instance of flask class
+
+app.config['SECRET_KEY'] = 'ab18fbcdabdabb67'
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route("/about")
+def about():
+    return render_template('about.html',title = 'About')
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title = 'Register', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title = 'Login', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug = True)
